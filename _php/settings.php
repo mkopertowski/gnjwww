7abd7e56d5640cba5d62c8aa281a795bd12e2f11
dzik <dzik@dzik>
2013-12-14 23:07:00 +0000

    obsluga zdjec
    Former-commit-id: 36f74b8ee867f8f6e14c96f8cc441decb2c53b1e

---
+++ _php/settings.php
@@ -0,0 +1,15 @@
+<?php 
+
+// general settings
+$ARTICLE_IMAGE_QUALITY = 50;    // in % for JPEG compression
+$ARTICLE_IMAGE_MAX_SIZE = 5000; // in kB, upload limit
+$ARTICLE_IMAGE_MAX_NUM  = 15;   // maximum number of images in article
+
+
+// ----------------------------------------------------------------------------
+// other
+$ARTICLE_IMAGE_WIDTH = 600;
+$ARTICLE_IMAGE_HEIGHT = 400;
+
+
+?>
