27b20dc321b3d5c596931ef96edd5181228f83e8
mirek <mirek>
2014-04-24 18:35:37 +0100

    max photos 15
    Former-commit-id: b2269eaf5e1f76bd8ae7ab5c58cde85b16c2fd82

--- _php/settings.php
+++ _php/settings.php
@@ -5,7 +5,7 @@ $ARTICLE_IMAGE_QUALITY = 60;        // in % for JPEG compression
 $ARTICLE_IMAGE_SMALL_QUALITY = 60;  // in % for JPEG compression
 $ARTICLE_IMAGE_NORMAL_QUALITY = 90;
 $ARTICLE_IMAGE_MAX_SIZE = 5000; // in kB, upload limit
-$ARTICLE_IMAGE_MAX_NUM  = 3;   // maximum number of images in article
+$ARTICLE_IMAGE_MAX_NUM  = 15;   // maximum number of images in article
 
 // ----------------------------------------------------------------------------
 // other
