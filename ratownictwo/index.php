881312c55ef937eb6568aeedb5558b2e11b1e543
Mirek Kopertowski <m.kopertowski@post.pl>
2015-01-05 22:29:56 +0100

    migracja
    Former-commit-id: ff2774844f6d3a658b08d0cd3c2f95fa49ca6809

--- ratownictwo/index.php
+++ ratownictwo/index.php
@@ -20,23 +20,10 @@ include('../_php/misc.php');
 		
 <!--============================= CONTENTS START ==========================================-->
 <h1>
-   RATOWNICTWO NURKOWE W JASKINIACH
+   RATOWNICTWO NURKOWE W JASKINIACH (RNJ)
 </h1>
 
 <?php
-/*
-//-----------------------------------------------------
-NewSection('','section');
-
-StartList();
-
-  ListItem('<b></b>');
-  ExtendedListItem('','','','','');
-  ExtendedListItem($title,$tekst,$link,$data,$autor);
-EndList();
-*/
-//-----------------------------------------------------
-//-----------------------------------------------------
 
 NewSection('SZKOLENIA','section');
 
@@ -45,54 +32,10 @@ StartList();
 $sqlfiltr="section='ratownictwo'";
 
 ArticleListFiltredMYSQL($mysqli,'..',$sqlfiltr);
-      
-  ExtendedListItem('Trening RNJ w kamieniołomie Zakrzówek',
-                   'W celu odpowiedniego przygotowania zespołu nurków jaskiniowych do manewrów ratownictwa zaplanowanych na przełom września i października, przeprowadzono ćwiczenia w kamieniołomie Zakrzówek...',
-                   '../ratownictwo/publication.php?id=2006zakrzowek2','RELACJA',
-                   '25. Listopad','Lucyna Cieślik');
 
-
+EndList();
 ?>
-	<li class="normal">
-      <b>PRÓBNA akcja ratownicza w Jaskini Zimnej.</b><br>
-      25. Lutego 2006. odbyła się PRÓBNA akcja ratownicza w Jaskini Zimnej.
-      Ratowaliśmy Pszczółka (Andrzej Szerszeń). W akcji uczetniczyło 
-      8 nurków jaskiniowych podzielonych na...[<a class="navismall" href="./ZimnaLuty2006.php">wiecej</a>]
-      <br><br><div align="right"><i>zamieszczono: 27. Luty 2006</i></div>
-              <div align="right"><i>autor: Mirek Kopertowski</i></div>
-	</li>
-	<li class="normal">
-      <b>Trening basenowy.</b><br>
-      9. Lutego 2006. odbyły się basenowe ćwiczenia ratownictwa
-      podczas których przećwiczono transport nurka w noszach. 
-      W zajęciach uczestniczyli: Lucyna Cieślik (SKTJ, pozostali SGW), 
-      Rafał Górecki, Mirek Kopertowski, Paweł Mordkowicz oraz Michał Stajszczyk oraz
-      w roli poszkodowanego: Ola Potulska (NZ)
-      [<a class="navismall" href="./basenLuty2006.php">wiecej</a>]
-      <br><br><div align="right"><i>zamieszczono: 12. Luty 2006</i></div>
-              <div align="right"><i>autor: Lucyna Cieślik</i></div>
-	</li>
-	<li class="normal">
-	   <b>Sympozjum nt. "Ratownictwo Nurkowe w Jaskiniach"</b><br>
-	   Sympozjum odbyło się w dniach 10-11 grudnia 2005 w Warszawie w siedzibie PZA.
-      Gościliśmy specjalistów z dziedziny
-      ratownictwa nurkowego ze Speleo Secours France - Jeana Pierra Baudu oraz
-      Oliviera Guerarda. Obecni też byli przedstawiciele Państwowej Straży
-      Pożarnej i innych służb ratowniczych.
-	   <br><br><div align="right"><i>zamieszczono: 12. Luty 2006</i></div>
-	</li>
-	<li class="normal">
-		<b>Stage Brancardage.</b><br>        
-      Podczas ostaniego weekendu listopada odbył się we Francji staż ratownictwa 
-      nurkowania jaskiniowego. Przećwiczone zostały techniki linowe wykorzystywane 
-      przy transporcie poszkodowanego w noszach, transport przez syfon 
-      (rownież w noszach) oraz omowiona zastała komunikacja z wykorzystaniem 
-      systemu Nicola i udzielanie pierwszej pomocy za syfonem (ASV). 
-      W stażu uczestniczyli: Rafał Górecki, Mirek Kopertowski (obaj SGW) 
-      oraz Andrzej Szerszeń (SW). 
-      <br><br><div align="right"><i>zamieszczono: 30. Listopad 2005</i></div>
-	</li>
-</ul>
+
 
 <!--============================= CONTENTS END   ==========================================-->
 <?php
