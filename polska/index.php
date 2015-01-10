9dc99b321d340ff086d2631fe9443def40a577da
Mirek Kopertowski <m.kopertowski@post.pl>
2015-01-10 15:36:51 +0100

    migracja
    Former-commit-id: 61bf706c9f5a4a2516f1867e16f55b60af4166ad

--- polska/index.php
+++ polska/index.php
@@ -6,7 +6,12 @@ if($bSubdir == true)
 	$sInclude = '.'.$sInclude;
 @include($sInclude);
 
+$GL_DIR = '..';
+
 @include('../_php/publication_supp.php');
+include('../_php/mysql.php');
+include('../_php/settings.php');
+include('../_php/misc.php');
 
 	renderHead($bSubdir,'','');
 	renderMenu($bSubdir,5,false,'POLSKA');
@@ -27,6 +32,20 @@ W Dziale zamieszczone są <b>relacje z nurkowań w Polsce</b>:
 	<li class="normal">w obiektach sztucznych: sztolniach, studniach i kopalniach.</li>
 
 </ul>
+
+<?php 
+
+NewSection('POZOSTAŁE POLSKIE NURKOWANIA','section');
+
+StartList();
+
+$sqlfiltr="section='polska' and tags=''";
+
+ArticleListFiltredMYSQL($mysqli,'..',$sqlfiltr);
+
+EndList();
+
+?>
 
 <!--============================= CONTENTS END   ==========================================-->
 <?php
