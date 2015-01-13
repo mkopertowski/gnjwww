fe07b90699dbbaa51298b940ddd57a44207ad37e
Mirek Kopertowski <m.kopertowski@post.pl>
2015-01-13 18:39:28 +0100

    migracja - english
    Former-commit-id: 1962c226426ed967f640009afe48dcccd3a57b56

--- english/cave_diving_poland.php
+++ english/cave_diving_poland.php
@@ -38,19 +38,9 @@ ArticleListFiltredMYSQL($mysqli,'..',$sqlfiltr);
 		&nbsp;&nbsp;&nbsp;[<a class="navismall" href="./syfony_tatry_en.php">see more...</a>]<br>&nbsp;
 	</li>
 	<li class="normal">
-		<b>Overview of cave diving in Poland</b><br>
-		Polish cave divers are not pampered by their homeland. Poland offers to them no great resurgences and only a limited number of sumps hidden deep in the caves. In fact, sump diving is the only form of cave diving available in Poland. Caves suitable for diving are to be found practically only in the Tatra mountains in the south of the country...
-		&nbsp;&nbsp;&nbsp;[<a class="navismall" href="./overview_pszczolek.php">see more...</a>]<br>&nbsp;
-	</li>
-	<li class="normal">
 		<b>Polish version of side-mount</b><br>
 		This type of bottle mounting is dedicated to narrow and short sumps. The main point in this configuration is to provide an easy way of attaching and detaching the bottle. The standard jacket with harness will not be used, because the area to be penetrated is constricted. A special belts are to be used...
 		&nbsp;&nbsp;&nbsp;[<a class="navismall" href="./sidemount_en.php">see more...</a>]<br>&nbsp;
-	</li>
-	<li class="normal">
-		<b>Cave diving in Poland at the end of XXth century</b><br>
-		Cave diving in Poland is not an easy job. There is no big resurgence in our country. Most diveable sumps are placed in Tatra Mountains on the southern border. There is an alpine type karst; the temperature is about 3 deg. C. The sumps are deep in the caves...
-		&nbsp;&nbsp;&nbsp;[<a class="navismall" href="./overview.php">see more...</a>]<br>&nbsp;
 	</li>
 	<li class="normal">
 		<b>Description of the rescue action in Izbucul de la Tauz, October 2002</b><br>
