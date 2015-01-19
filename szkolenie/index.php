13f288737ee90612bc75cedca53c221a32cac400
Mirek Kopertowski <m.kopertowski@post.pl>
2015-01-19 22:50:44 +0100

    migracja
    Former-commit-id: e68419db3650f4dc63f46afe3b2797c234ded529

--- szkolenie/index.php
+++ szkolenie/index.php
@@ -31,35 +31,7 @@ $sqlfiltr="section='technika'";
 ArticleListFiltredMYSQL($mysqli,'..',$sqlfiltr);
 
 ?>
-	<li class="normal">
-		<b>Lista sprzętu potrzebnego na Warsztatach Nurkowania Jaskiniowego</b><br>
-		&nbsp;&nbsp;&nbsp;[<a class="navismall" href="./listasprzetu.php">więcej...</a>]<br>&nbsp;
-	</li>
-	<li class="normal">
-		<b>Planowanie ilości mieszanek oddechowych</b><br>
-		Poniższe rozważania dotyczą oddychania powietrzem. Planujemy, że podczas nurkowania przeznaczymy taka sama ilość powietrza na penetrację, powrót i rezerwę. Taki podział określa się mianem reguły jednej trzeciej...
-		&nbsp;&nbsp;&nbsp;[<a class="navismall" href="./iloscmieszanek.php">więcej...</a>]<br>&nbsp;
-	</li>
-	<li class="normal">
-		<b>System mocowania butli po bokach</b><br>
-		Ten sposób mocowania jest przewidziany do ciasnych i krótkich syfonów. Jego głównym założeniem jest możliwość łatwego przypinania i odpinania butli. Ze względu na przewidywane ciasności standardowy jacket nie będzie używany. Wobec tego należy użyć specjalnych taśm do mocowania...
-		&nbsp;&nbsp;&nbsp;[<a class="navismall" href="./sidemount_pl.php">więcej...</a>]<br>&nbsp;
-	</li>
-	<li class="normal">
-		<b>Automaty</b><br>
-		W nurkowaniu jaskiniowym zawsze używa się dwu niezależnych butli. Dobrze jest jeżeli jeden z drugich stopni automatów jest prawy a drugi lewy. Każdy z automatów należy przygotować w podobny sposób...
-		&nbsp;&nbsp;&nbsp;[<a class="navismall" href="./automat.php">więcej...</a>]<br>&nbsp;
-	</li>
-	<li class="normal">
-		<b>Kołowrotek</b><br>
-		Zasadniczym wyposażeniem każdego nurka jaskiniowego jest kołowrotek. Jego konstrukcja jak i przeznaczenie różni się istotnie od kołowrotków używanych podczas nurkowania w morzu. Poręczówka w jaskini służy do wskazywania kierunku powrotu w przypadku zmącenia wody...
-		&nbsp;&nbsp;&nbsp;[<a class="navismall" href="./kolowrotek.php">więcej...</a>]<br>&nbsp;
-	</li>
-	<li class="normal">
-		<b>Technika - Temperatura mieszanki oddechowej za pierwszym stopniem automatu</b><br>
-		Celem niniejszego artykułu jest pokazanie praktycznego sposobu na wyznaczanie temperatury mieszanki oddechowej po rozprężeniu na pierwszym stopniu automatu...
-		&nbsp;&nbsp;&nbsp;[<a class="navismall" href="./efektjt.php">więcej...</a>]<br>&nbsp;
-	</li>
+
 </ul>
 <!--============================= CONTENTS END   ==========================================-->
 <?php
