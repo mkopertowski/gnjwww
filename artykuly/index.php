04de959ee460f890f3a1468ebfe70fd7e0c06950
Mirek Kopertowski <m.kopertowski@post.pl>
2015-04-02 22:08:11 +0200

    migracja
    Former-commit-id: 161e025b848a696162ee520d02e7f6ef076e6b5d

--- artykuly/index.php
+++ artykuly/index.php
@@ -49,37 +49,6 @@ StartList();
                      
 ?>
 
-	<li class="normal">
-		<b>Rekord w Bushmansgat</b><br>
-
-		W dniu 11 września 2003 Leszek Czarnecki
-ustanowił nowy rekord Polski w nurkowaniu jaskiniowym, zanurzając się na
-głębokość 194 m w jaskini Boesmansgat (Bushmansgat) w RPA (1560 m n.p.m.).
-		&nbsp;&nbsp;&nbsp;[<a class="navismall" href="./bushmansgat2003.php">więcej...</a>]<br>&nbsp;
-	</li>
-	<li class="normal">
-		<b>Wypadek w jaskini Isverna (Rumunia)</b><br>
-		W dniu 15 listopada 2002 roku w rumuńskiej jaskini Isverna (góry Muntii Mehedinti) zdarzył się wypadek...
-		&nbsp;&nbsp;&nbsp;[<a class="navismall" href="./isverna2002.php">więcej...</a>]<br>&nbsp;
-
-	</li>
-	<li class="normal">
-		<b>Wypadek w Grocie Scaletta na Przylądku Iacco (Capo Palinuro)</b><br>
-		Dnia 10 .09.1996 o 17.30 trzech płetwonurków z Polski : Grzegorz Sosinka, Rafał Spyrka i Witold Olszowski, przebywający już od miesiąca we Włoszech na wyprawie, wpływają do jaskini Scaletta...
-		&nbsp;&nbsp;&nbsp;[<a class="navismall" href="./scalet1.php">więcej...</a>]<br>&nbsp;
-	</li>
-	<li class="normal">
-
-		<b>Wypadek w Grocie Scaletta - inna relacja</b><br>
-		Dnia 10.09.196 trzech płetwonurków zginęło w grocie morskiej Scaletta niedaleko Palinuro. Dwa ciała zostały wydobyte...
-		&nbsp;&nbsp;&nbsp;[<a class="navismall" href="./scalett2.php">więcej...</a>]<br>&nbsp;
-	</li>
-	<li class="normal">
-		<b>Wypadek w Sotano de San Augustin</b><br>
-		Ian Rolland, jeden z najbardziej doświadczonych brytyjskich płetwonurków jaskiniowych, zginął 27 marca 1994 eksplorując ostatni syfon w Sotano de San Augustin w Meksyku...
-		&nbsp;&nbsp;&nbsp;[<a class="navismall" href="./sotdesan.php">więcej...</a>]<br>&nbsp;
-
-	</li>
 </ul>
 
 <!--============================= CONTENTS END   ==========================================-->
