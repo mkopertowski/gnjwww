56766dc589c0c6c5a452133958ddca5f99a57d6a
Mirek Kopertowski <m.kopertowski@post.pl>
2016-07-11 23:22:50 +0200

    strefagnj - podglad artykulu
    Former-commit-id: 84587b2e7e62ed1a5f84dd6bf7c1ffa3d4f633b4

--- strefagnj/article_show.php
+++ strefagnj/article_show.php
@@ -9,7 +9,7 @@ include("../_php/mysql.php");
 include("../_php/misc.php");
 include("../_php/Renderer.php");
 include("../_php/settings.php");
-require_once("../_php/wikiParser.php");
+include("../_php/components.php");
 
 $authorid = $_SESSION['userid'];
 $id = $_REQUEST['id'];
@@ -21,18 +21,59 @@ $result=$mysqli->query($sql);
 $row = $result->fetch_assoc();
 
 if(($_SESSION['usertype'] == "admin") || ($row['authorid'] == $authorid))
-{
+{	
 	switch($row['layout'])
 	{
 		case "ex":
-			ShowArticleEx($mysqli,$row,$id,false);
+	        $Article = new Renderer("../_tpl/article.ex.tpl.php");
+			PrepareArticleEx($mysqli,$row,$id,true,$Article);
 			break;
 		case "nofb":
-			ShowArticle($mysqli,$row,$id,false);
-			break;
+			$Article = new Renderer("../_tpl/article.tpl.php");
+			PrepareArticle($mysqli,$row,$id,false,$Article);
+			break;	
 		default:
-			ShowArticle($mysqli,$row,$id,false);
+			if($row['section'] == "plany i przekroje")
+			{
+				$Article = new Renderer("../_tpl/planprzekroj.tpl.php");
+			}
+			else
+			{
+				$Article = new Renderer("../_tpl/article.tpl.php");
+			}
+			PrepareArticle($mysqli,$row,$id,true,$Article);
 	}
-}	
+		
+	$Article->set("mysqli",$mysqli);
+	$Article->set("dots","..");
+
+	/* decide which layout to use */
+	if(($row['section'] == "odeszli") || ($row['section'] == "plany i przekroje")) 
+	{
+		$ArticleLayout = new Renderer("../_tpl/simpleLayout.tpl.php");
+	}
+	else 
+	{
+		$sqlfiltr = "id != ".$id;
+		$sqlfiltr.= " AND section='".$row['section']."'";
+		$limit = "LIMIT 3";
+		$result=ArticleListFiltredMYSQL($mysqli,$sqlfiltr,$limit);
+		
+		$ArticleLayout = new Renderer("../_tpl/advancedLayout.tpl.php");
+		$ArticleLayout->set("dots","..");
+		$ArticleLayout->set("panelArticles",$result);
+		$ArticleLayout->set("mysqli",$mysqli);
+	}
+		
+	$ArticleLayout->set("article",$Article->parse());
+	
+	$Page = new Renderer("../_tpl/gnj.tpl.php");
+	$Page->set("Content",$ArticleLayout->parse());
+	$Page->set("dots","..");
+	$Page->set("mysqli",$mysqli);
+	
+	$Page->publish();
+	
+}		
 
 ?>
